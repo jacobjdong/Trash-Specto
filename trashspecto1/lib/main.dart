6da6f4a99d036bbc71602dc64215f6d25a44aa58
jacobjdong <jacobjdong@gmail.com>
2020-04-24 12:37:36 -0500

    Pushing Skeleton App
    here's a framework

---
+++ trashspecto1/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xfff7d0e5),
+        accentColor: Color(0xff9ee493),
+        
+        fontFamily: 'Georgia'
+      ),
+      home: new ScreenOne(),
+    );
+  }
+}
+
+class ScreenOne extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Center(child: new Text ("Home Page", textAlign: TextAlign.center)),
+      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        children: <Widget>[
+          RaisedButton(
+            child: Text(
+              'Overall Garbage',
+              style: TextStyle(fontSize: 20)
+            ),
+            onPressed: () {
+              Navigator.push(
+                ctxt,
+                MaterialPageRoute(builder: (ctxt) => ScreenTrash()),
+              );
+            }
+          ),
+          RaisedButton(
+            child: Text(
+              'Wastewater',
+              style: TextStyle(fontSize: 20)
+            ),
+            onPressed: () {
+              Navigator.push(
+                ctxt,
+                MaterialPageRoute(builder: (ctxt) => ScreenWastewater()),
+              );
+            }
+          ),
+          RaisedButton(
+            child: Text(
+              'Food Waste',
+              style: TextStyle(fontSize: 20)
+            ),
+            onPressed: () {
+              Navigator.push(
+                ctxt,
+                MaterialPageRoute(builder: (ctxt) => ScreenFoodWaste()),
+              );
+            }
+          ),
+        ]
+      )
+    );
+  }
+}
+
+class ScreenTrash extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
+          onPressed: () {
+            Navigator.pop(ctxt);
+          }
+        ),
+        title: new Center(child: new Text ("Screen Trash", textAlign: TextAlign.center)),
+      ),
+      body: new Text("Here is where the total trash form will be"),
+    );
+  }
+}
+
+class ScreenWastewater extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
+          onPressed: () {
+            Navigator.pop(ctxt);
+          }
+        ),
+        title: new Center(child: new Text ("Screen Wastewater", textAlign: TextAlign.center)),
+      ),
+      body: new Text("Here is where the wastewater form will be"),
+    );
+  }
+}
+
+class ScreenFoodWaste extends StatelessWidget {
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
+          onPressed: () {
+            Navigator.pop(ctxt);
+          }
+        ),
+        title: new Center(child: new Text ("Screen Food Waste", textAlign: TextAlign.center)),
+      ),
+      body: new Text("Here is where the food waste form will be"),
+    );
+  }
+}
