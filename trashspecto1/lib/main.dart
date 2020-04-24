136dfe6e0b8b7e48753d16ed2f2fc1b0dd85e9bb
jacobjdong <jacobjdong@gmail.com>
2020-04-24 15:36:09 -0500

    Stateful Sliders
    Added a slider that kinda works

--- trashspecto1/lib/main.dart
+++ trashspecto1/lib/main.dart
@@ -83,20 +83,82 @@ class ScreenOne extends StatelessWidget {
   }
 }
 
-class ScreenTrash extends StatelessWidget {
+class ScreenTrash extends StatefulWidget {
+  @override
+  _ScreenTrashState createState() => _ScreenTrashState();
+}
+
+class _ScreenTrashState extends State<ScreenTrash> {
+  double _value = 2;
+
   @override
   Widget build (BuildContext ctxt) {
     return new Scaffold(
       appBar: new AppBar(
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
           onPressed: () {
             Navigator.pop(ctxt);
           }
         ),
-        title: new Center(child: new Text ("Screen Trash", textAlign: TextAlign.center)),
+        title: new Text ("Screen Trash", textAlign: TextAlign.center),
       ),
-      body: new Text("Here is where the total trash form will be"),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '0',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff778BF3),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SliderTheme(
+                   data: SliderTheme.of(ctxt).copyWith(
+                    activeTrackColor: Color(0xff778BF3),
+                    inactiveTrackColor: Color(0xffd5e1fd),
+                    trackShape: RectangularSliderTrackShape(),
+                    trackHeight: 4.0,
+                    thumbColor: Color(0xff778BF3),
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
+                  ),
+                  child: Slider(
+                    value: _value,
+                    onChanged: (value) {
+                      setState(() {
+                        _value = value;
+                        print(value);
+                      });
+                    },
+                    min: 0,
+                    max: 7,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                '7',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff778BF3),
+                ),
+              ),
+            ]
+          ),
+        ),
+      )
     );
   }
 }
@@ -106,13 +168,14 @@ class ScreenWastewater extends StatelessWidget {
   Widget build (BuildContext ctxt) {
     return new Scaffold(
       appBar: new AppBar(
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
           onPressed: () {
             Navigator.pop(ctxt);
           }
         ),
-        title: new Center(child: new Text ("Screen Wastewater", textAlign: TextAlign.center)),
+        title: new Text ("Screen Wastewater", textAlign: TextAlign.center),
       ),
       body: new Text("Here is where the wastewater form will be"),
     );
@@ -124,13 +187,14 @@ class ScreenFoodWaste extends StatelessWidget {
   Widget build (BuildContext ctxt) {
     return new Scaffold(
       appBar: new AppBar(
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
           onPressed: () {
             Navigator.pop(ctxt);
           }
         ),
-        title: new Center(child: new Text ("Screen Food Waste", textAlign: TextAlign.center)),
+        title: new Text ("Screen Food Waste", textAlign: TextAlign.center),
       ),
       body: new Text("Here is where the food waste form will be"),
     );
@@ -142,13 +206,14 @@ class ScreenARViewer extends StatelessWidget {
   Widget build (BuildContext ctxt) {
     return new Scaffold(
       appBar: new AppBar(
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
           onPressed: () {
             Navigator.pop(ctxt);
           }
         ),
-        title: new Center(child: new Text ("Screen AR Viewer", textAlign: TextAlign.center)),
+        title: new Text ("Screen AR Viewer", textAlign: TextAlign.center),
       ),
       body: new Text("Heres wehre the AR view would be"),
     );
