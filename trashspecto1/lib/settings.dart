e3bd41a905e49dbecce899dbc83777b33e7e5a0b
Emilio <erogalla@utexas.edu>
2020-04-24 20:42:37 -0500

    added files

---
+++ trashspecto1/lib/settings.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  final String region;
+  final int metric;
+  final Function updateRegion;
+  final Function updateMetric;
+  Settings({this.region, this.metric, this.updateRegion, this.updateMetric});
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+
+
+
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xff9ee493)),
+          onPressed: () {
+            Navigator.pop(ctxt);
+          }
+        ),
+        title: new Text ("Settings", textAlign: TextAlign.center),
+      ),
+      body: new ListView(
+       children: [
+          new Column(
+            children: <Widget>[
+              new DropdownButton<String>(
+                value: widget.region,
+                onChanged: (String newValue) {
+                  setState(() {
+                    this.region = newValue;
+                  });
+                },
+                items: <String>['North America', 'South America', 'Asia', 'Europe', 'Africa'].map((String value) {
+                  return new DropdownMenuItem<String>(
+                    value: value,
+                    child: new Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ],
+      )
+      
+    );
+  }
+}
---
+++ trashspecto1/lib/trash.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'settings.dart';
+import 'food.dart';
+import 'trash.dart';
+import 'water.dart';
+
+class ScreenTrash extends StatefulWidget {
+  @override
+  _ScreenTrashState createState() => _ScreenTrashState();
+}
+
+class _ScreenTrashState extends State<ScreenTrash> {
+  double _value = 2;
+
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xff778BF3)),
+          onPressed: () {
+            Navigator.pop(ctxt);
+          }
+        ),
+        title: new Text ("Screen Trash", textAlign: TextAlign.center),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '0',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff778BF3),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SliderTheme(
+                   data: SliderTheme.of(ctxt).copyWith(
+                    activeTrackColor: Color(0xff778BF3),
+                    inactiveTrackColor: Color(0xffd5e1fd),
+                    trackShape: RectangularSliderTrackShape(),
+                    trackHeight: 4.0,
+                    thumbColor: Color(0xff778BF3),
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
+                  ),
+                  child: Slider(
+                    value: _value,
+                    onChanged: (value) {
+                      setState(() {
+                        _value = value;
+                        print(value);
+                      });
+                    },
+                    min: 0,
+                    max: 7,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                '7',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff778BF3),
+                ),
+              ),
+            ]
+          ),
+        ),
+      )
+    );
+  }
+}
