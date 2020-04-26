65fd1fb1f310e9dfa74134af24c00928970d07cc
jacobjdong <jacobjdong@gmail.com>
2020-04-25 22:28:09 -0500

    new page: main garbo

--- trashspecto1/lib/trash.dart
+++ trashspecto1/lib/trash.dart
@@ -7,89 +7,263 @@ class ScreenTrash extends StatefulWidget {
 }
 
 class _ScreenTrashState extends State<ScreenTrash> {
-  double _value = 2;
+  double _recyclePercent = 10, _clothing = 1, _compostPercent = 10;
+  String _shoppingType;
+  String newValue;
+  List<Widget> cards;
+  int visible = 4;
 
+  dynamic _background = "images/citylayout.png";
+  
   @override
   Widget build (BuildContext ctxt) {
-    return new Scaffold(
-      appBar: new AppBar(
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Color(0xff778BF3)),
-          onPressed: () {
-            Navigator.pop(ctxt);
-          }
+    updateCards();
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(_background),
+          fit: BoxFit.cover
         ),
-        title: new Text ("Screen Trash", textAlign: TextAlign.center),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Container(
-          child: Row(
-            children: <Widget>[
-              Expanded(child: 
-                customSlider(
-                  updateVal: (value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
-                  question: "OI MATE PISS OFF",
-                  min: 0,
-                  max: 2,
-                  showerTime: 1.0,
-                )
+      child: new Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: new AppBar(
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Color(0xff778BF3)),
+            onPressed: () {
+              Navigator.pop(ctxt);
+            }
+          ),
+          title: new Text ("Screen Garbage", textAlign: TextAlign.center),
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: cards,
               ),
-                
-              Text(
-                '0',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xff778BF3),
-                ),
-              ),
-              Column(
+        )
+      ),
+    );
+  }
+
+  List<Widget> updateCards() {
+    cards = new List();
+
+    cards.insert(0,
+      Positioned(
+        top: 130,
+        child: Card(
+          elevation: 10,
+          color: Color.fromARGB(255, 240, 230, 230),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Container(
+            width: 260.0,
+            height: 350.0,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  SliderTheme(
-                   data: SliderTheme.of(ctxt).copyWith(
-                    activeTrackColor: Color(0xff778BF3),
-                    inactiveTrackColor: Color(0xffd5e1fd),
-                    trackShape: RectangularSliderTrackShape(),
-                    trackHeight: 4.0,
-                    thumbColor: Color(0xff778BF3),
-                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
-                    overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
-                  ),
-                  child: Slider(
-                    value: _value,
-                    onChanged: (value) {
+                   customSlider(
+                    updateVal: (value) {
                       setState(() {
-                        _value = value;
-                        print(value);
+                        this._recyclePercent = value;
                       });
                     },
+                    question: "What percentage of your waste do you recycle",
                     min: 0,
-                    max: 7,
+                    max: 100,
+                    showerTime: this._recyclePercent,
+                    units: "%",
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      'Remove!',
+                      style: TextStyle(fontSize: 20)
                     ),
+                    onPressed: () {
+                      setState(() {
+                        visible--;
+                      });
+                    }
                   ),
                 ],
               ),
-              Text(
-                '7',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xff778BF3),
-                ),
-              ),
-            ]
+            ),
           ),
         ),
-      )
+      ),
     );
+
+    cards.insert(0,
+      Positioned(
+        top: 100,
+        child: Card(
+          elevation: 10,
+          color: Color.fromARGB(255, 240, 230, 230),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Container(
+            alignment: Alignment.center,
+            width: 260.0,
+            height: 350.0,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                   customSlider(
+                    updateVal: (value) {
+                      setState(() {
+                        this._compostPercent = value;
+                      });
+                    },
+                    question: "How much of your food waste do you compost",
+                    min: 0,
+                    max: 100,
+                    showerTime: this._compostPercent,
+                    units: "%"
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      'Remove!',
+                      style: TextStyle(fontSize: 20)
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        visible--;
+                      });
+                    }
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    cards.insert(0,
+      Positioned(
+        top: 70,
+        child: Card(
+          elevation: 10,
+          color: Color.fromARGB(255, 240, 230, 230),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Container(
+            width: 260.0,
+            height: 350.0,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'What type of shopper are you?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    hint: Text('Average'),
+                    onChanged: (String changedValue) {
+                      newValue = changedValue;
+                      _shoppingType = changedValue;
+                      setState(() {
+                        newValue;
+                        print(newValue);
+                      });
+                    },
+                    value: newValue,
+                    items: <String>['Frugal', 'Average', 'Shopaholic'].map((String value) {
+                      return new DropdownMenuItem<String>(
+                        value: value,
+                        child: new Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      'Remove!',
+                      style: TextStyle(fontSize: 20)
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        visible--;
+                      });
+                    }
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    cards.insert(0,
+      Positioned(
+        top: 40,
+        child: Card(
+          elevation: 10,
+          color: Color.fromARGB(255, 240, 230, 230),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Container(
+            width: 260.0,
+            height: 350.0,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                   customSlider(
+                    updateVal: (value) {
+                      setState(() {
+                        this._clothing = value;
+                      });
+                    },
+                    question: "What percent of your clothing do you donate",
+                    min: 0,
+                    max: 100,
+                    showerTime: this._clothing,
+                    units: "%"
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      'Remove!',
+                      style: TextStyle(fontSize: 20)
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        visible--;
+                      });
+                    }
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    for (int i = cards.length; i > visible; i--) {
+      cards.removeAt(i-1);
+    }
+
   }
 }
